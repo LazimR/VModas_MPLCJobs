455102ea491d3836bc4112bf1596b50063422aad
LazimR <lrodrigues356@gmail.com>
2023-02-20 14:54:10 -0300

    Versão 2

--- VitalModas/js/carousel.js
+++ VitalModas/js/carousel.js
@@ -58,12 +58,3 @@ $(document).ready(function() {
       }
     });
   });
-
-  window.onscroll = function() {
-    var navbar = document.getElementById("navbar");
-    if (window.pageYOffset > 5) {
-      navbar.classList.add("fixed-top");
-    } else {
-      navbar.classList.remove("fixed-top");
-    }
-  }
